d022a0c40c4ad5d59d231693d9b1bca6524204ae
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-07-13 01:58:58 +0700

    Update __google.py

--- LaylaRobot/modules/__google.py
+++ LaylaRobot/modules/__google.py
@@ -267,7 +267,7 @@ async def apk(e):
             + app_link
             + "'>View in Play Store</a>"
         )
-        app_details += "\n\n===> @AwesomeSupport <==="
+        app_details += "\n\n===> @OdaSupport <==="
         await e.reply(app_details, link_preview=True, parse_mode="HTML")
     except IndexError:
         await e.reply("No result found in search. Please enter **Valid app name**")
@@ -275,7 +275,7 @@ async def apk(e):
         await e.reply("Exception Occured:- " + str(err))
 
 
-__mod_name__ = "◎Search"
+__mod_name__ = "Search"
 
 __help__ = """
  ❍ /google <text>*:* Perform a google search
@@ -287,5 +287,5 @@ __help__ = """
  ❍ /country <country name>*:* Gathering info about given country
  ❍ /imdb <Movie name>*:* Get full info about a movie with imdb.com
  ❍ Yone <query>*:* Yone answers the query
-  💡Ex: `Yone where is India?`
+  💡Ex: `Yone where is Indonesia?`
 """
