ef6771661ca3ae0bb0308281536976cbae8fc554
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-07-13 18:41:40 +0700

    Update gtranslator.py

--- LaylaRobot/modules/gtranslator.py
+++ LaylaRobot/modules/gtranslator.py
@@ -1,5 +1,5 @@
 from emoji import UNICODE_EMOJI
-from google_trans_new import LANGUAGES, google_translator, Translator
+from google_trans_new import LANGUAGES, google_translator
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
