b195fe9dc5e9025a3afca7dd7e9c2c1583d5e0d1
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-07-13 17:01:24 +0700

    Create arqapi.py

---
+++ LaylaRobot/utils/arqapi.py
@@ -0,0 +1,28 @@
+import json
+import sys
+from random import randint
+from time import time
+
+import aiohttp
+from aiohttp import ClientSession
+from googletrans import Translator
+from Python_ARQ import ARQ
+from search_engine_parser import GoogleSearch
+
+from LaylaRobot import BOT_ID, OWNER_ID
+from LaylaRobot import pbot
+
+ARQ_API = "WZQUBA-PFAZQJ-OMIINH-MIVHYM-ARQ"
+ARQ_API_KEY = "WZQUBA-PFAZQJ-OMIINH-MIVHYM-ARQ"
+SUDOERS = OWNER_ID
+ARQ_API_URL = "https://thearq.tech"
+
+# Aiohttp Client
+print("[INFO]: INITIALZING AIOHTTP SESSION")
+aiohttpsession = ClientSession()
+# ARQ Client
+print("[INFO]: INITIALIZING ARQ CLIENT")
+arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
+
+app = pbot
+import socket
