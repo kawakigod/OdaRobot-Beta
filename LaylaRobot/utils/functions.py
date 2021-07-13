626ce4afa7c7c4b36467c08cbab0740360365b13
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-07-14 04:31:11 +0700

    Update functions.py

--- LaylaRobot/utils/functions.py
+++ LaylaRobot/utils/functions.py
@@ -7,7 +7,7 @@ from random import randint
 from re import findall
 from time import time
 from datetime import timedelta, datetime
-import aiosession
+from LaylaRobot import aiohttpsession as aiosession
 import aiofiles
 import aiohttp
 import speedtest
