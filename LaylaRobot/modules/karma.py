6f36e39f1cfe091d33f89d5725d1ae7acd7369fe
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-07-13 14:11:11 +0700

    Update karma.py

--- LaylaRobot/modules/karma.py
+++ LaylaRobot/modules/karma.py
@@ -1,7 +1,7 @@
 from LaylaRobot import pbot
-from LaylaRobot.utlis.dbfunc import (update_karma, get_karma, get_karmas,
+from LaylaRobot.utils.dbfunc import (update_karma, get_karma, get_karmas,
                                    int_to_alpha, alpha_to_int)
-from LaylaRobot.utlis.filter_groups import karma_positive_group, karma_negative_group
+from LaylaRobot.utils.filter_groups import karma_positive_group, karma_negative_group
 from pyrogram import filters
 
 
