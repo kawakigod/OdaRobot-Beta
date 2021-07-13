392dfdfbbb42cbd1c0f2f246b471454b645c536c
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-07-13 12:54:43 +0700

    Update permissions.py

--- LaylaRobot/utils/permissions.py
+++ LaylaRobot/utils/permissions.py
@@ -79,20 +79,4 @@ def adminsOnly(permission):
                 return await unauthorised(
                     message, permission, subFunc2
                 )
-            # For admins and sudo users
-            userID = message.from_user.id
-            permissions = await member_permissions(chatID, userID)
-            if (
-                userID not in SUDOERS
-                and permission not in permissions
-            ):
-                return await unauthorised(
-                    message, permission, subFunc2
-                )
-            return await authorised(
-                func, subFunc2, client, message, *args, **kwargs
-            )
-
-        return subFunc2
-
-    return subFunc
+            
