2b478404ca7956351f9b5dd0cb488d394e19f994
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-07-14 05:04:52 +0700

    Update arqapi.py

--- LaylaRobot/utils/arqapi.py
+++ LaylaRobot/utils/arqapi.py
@@ -11,7 +11,7 @@ from google_trans_new import google_translator
 from Python_ARQ import ARQ
 from search_engine_parser import GoogleSearch
 
-from LaylaRobot import BOT_ID, OWNER_ID
+from LaylaRobot import BOT_ID, OWNER_ID, ARQ_API_URL, ARQ_API_KEY
 from LaylaRobot import pbot
 
 ARQ_API = "WZQUBA-PFAZQJ-OMIINH-MIVHYM-ARQ"
