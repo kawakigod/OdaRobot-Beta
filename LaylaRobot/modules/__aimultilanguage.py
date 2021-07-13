2cc3e49be375b6035bdf93c266ede77c8a5bf1f8
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-07-14 04:15:15 +0700

    Update __aimultilanguage.py

--- LaylaRobot/modules/__aimultilanguage.py
+++ LaylaRobot/modules/__aimultilanguage.py
@@ -29,7 +29,6 @@ from pyrogram import filters
 
 from LaylaRobot import BOT_ID
 from LaylaRobot.helper_extra.aichat import add_chat, get_session, remove_chat
-from LaylaRobot.utils.arqapi import arq
 from LaylaRobot.utils.permissions import adminsOnly
 from LaylaRobot import pbot as oda
 
