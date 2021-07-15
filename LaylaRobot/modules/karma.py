c61f2d821540f4c9303668cabbe9db8e78022fc9
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-07-16 04:37:35 +0700

    Update karma.py

--- LaylaRobot/modules/karma.py
+++ LaylaRobot/modules/karma.py
@@ -40,7 +40,7 @@ async def get_karmas_count() -> dict:
         return {}
     chats_count = 0
     karmas_count = 0
-    for chat in await chats.to_list(length=1000000):
+    for chat in chats.to_list(length=1000000):
         for i in chat["karma"]:
             karmas_count += chat["karma"][i]["karma"]
         chats_count += 1
@@ -48,7 +48,7 @@ async def get_karmas_count() -> dict:
 
 
 async def get_karmas(chat_id: int) -> Dict[str, int]:
-    karma = await karmadb.find_one({"chat_id": chat_id})
+    karma = karmadb.find_one({"chat_id": chat_id})
     if karma:
         karma = karma["karma"]
     else:
@@ -58,14 +58,14 @@ async def get_karmas(chat_id: int) -> Dict[str, int]:
 
 async def get_karma(chat_id: int, name: str) -> Union[bool, dict]:
     name = name.lower().strip()
-    karmas = await get_karmas(chat_id)
+    karmas = get_karmas(chat_id)
     if name in karmas:
         return karmas[name]
 
 
 async def update_karma(chat_id: int, name: str, karma: dict):
     name = name.lower().strip()
-    karmas = await get_karmas(chat_id)
+    karmas = get_karmas(chat_id)
     karmas[name] = karma
     await karmadb.update_one(
         {"chat_id": chat_id}, {"$set": {"karma": karmas}}, upsert=True
