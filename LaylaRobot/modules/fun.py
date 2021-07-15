7a30c4d5d03e11fff3834b52032a795a434d7966
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-07-16 03:58:01 +0700

    Update fun.py

--- LaylaRobot/modules/fun.py
+++ LaylaRobot/modules/fun.py
@@ -12,7 +12,6 @@ from telegram import ChatPermissions, ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, run_async, CommandHandler, Filters
 
-import LaylaRobot.modules.helper_funcs.string_store as fun
 
 GIF_ID = "CgACAgQAAx0CSVUvGgAC7KpfWxMrgGyQs-GUUJgt-TSO8cOIDgACaAgAAlZD0VHT3Zynpr5nGxsE"
 
