8c2da9ffda43b6b85a519b78dbf16a3008a1244e
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-07-16 03:14:13 +0700

    Update backups.py

--- LaylaRobot/modules/backups.py
+++ LaylaRobot/modules/backups.py
@@ -341,8 +341,8 @@ def export_data(update, context):
         pass
     context.bot.sendDocument(
         current_chat_id,
-        document=open("LaylaRobot{}.backup".format(chat_id), "rb"),
-        caption="💾*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `LaylaRobot-Backup` was specially made for notes 📚.".format(
+        document=open("OdaRobot{}.backup".format(chat_id), "rb"),
+        caption="💾*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `OdaRobot-Backup` was specially made for notes 📚.".format(
             chat.title, chat_id, tgl
         ),
         timeout=360,
